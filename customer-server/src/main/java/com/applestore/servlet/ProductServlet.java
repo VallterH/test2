2861a5a11f4448d358e06e27ebaf31d6903230b5
Igor <igormalyutinh@gmail.com>
2025-11-03 19:16:03 +0000

    servelet/ apache tomcat 7 plugin, gson - json, web.xml

--- customer-server/src/main/java/com/applestore/servlet/ProductServlet.java
+++ customer-server/src/main/java/com/applestore/servlet/ProductServlet.java
@@ -5,20 +5,24 @@ import com.applestore.model.Product;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet("/products")
 public class ProductServlet extends HttpServlet {
+
+    private final productdao dao = new productdao();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
         resp.setContentType("application/json; charset=UTF-8");
-
-        productdao dao = new productdao();
         List<Product> products = dao.getAllProducts();
-
         String json = new Gson().toJson(products);
         resp.getWriter().write(json);
     }
